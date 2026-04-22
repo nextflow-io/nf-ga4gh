111d1135451ff01ac7ca76dc5628b541c6349723
Javed Habib <jh4official@gmail.com>
2026-04-22 16:09:59 +0530

    fix: failing test due to missing mock stub
    Signed-off-by: Javed Habib <jh4official@gmail.com>

--- src/test/nextflow/ga4gh/tes/executor/TesTaskHandlerTest.groovy
+++ src/test/nextflow/ga4gh/tes/executor/TesTaskHandlerTest.groovy
@@ -43,6 +43,7 @@ class TesTaskHandlerTest extends Specification {
         }
         task.getName() >> 'tes-task'
         task.getWorkDir() >> Paths.get(".")
+        task.getTargetDir() >> Paths.get(".")
         task.getConfig() >> new TaskConfig(memory: '2GB', cpus: 4, disk: '10GB')
         task.getContainer() >> 'foo'
         def handler = new TesTaskHandler(task, executor)
